1c6515b929f34ae8735d0baccf234d254f3c0f45
jaydeep <jaydeeppatidar87@gmail.com>
2020-06-08 18:07:15 +0530

    final commit in file frequency of all elements

--- frequencyofelements.py
+++ frequencyofelements.py
@@ -1,19 +1,13 @@
 print('print distinct list of elements along with their frequency of occurence\n Enter a list')
 l=eval(input())
-l1=l
 length=len(l)
-for x in l1:
-    i=0
+fr=0
+for x in l:
     count=0
     for y in l:
         if y==x:
             count+=1
-            #l.pop(i)
-            i+=1
-            continue
     else:
-        fr=(count/length)*100
-        print('frequncy of element',x,'is',fr,'%')
-        i+=1
-input()
-        
+         fr=(count/length)*100
+         print('frequncy of element',x,'is',fr,'%')
+input('enter press to exit')
